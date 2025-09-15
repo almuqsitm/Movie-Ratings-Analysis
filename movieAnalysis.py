ac4de9ca9480de2ec0806cfed11c10cf9d46dd83
Mohammed Al-Muqsit <almuqsit101@gmail.com>
2025-09-14 20:11:21 -0400

    dark background for deploymen

--- movieAnalysis.py
+++ movieAnalysis.py
@@ -10,6 +10,13 @@ st.set_page_config(
     page_icon="🎬",
     layout="wide",
     initial_sidebar_state="expanded",
+    theme={
+        "primaryColor": "#f63366",
+        "backgroundColor": "#111111",
+        "secondaryBackgroundColor": "#393939",
+        "textColor": "#fafafa",
+        "font": "sans serif",
+    },
 )
 
 
